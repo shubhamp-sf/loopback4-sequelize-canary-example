369b19821539ccb09cc75cf60ebd6cbe79c1cb50
Shubham P <shubham.prajapat@sourcefuse.com>
2023-06-06 14:06:13 +0530

    lb4 scaffold

---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    default: 18,
+  })
+  age?: number;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  active?: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
